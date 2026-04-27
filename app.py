9c8944da12a907bcee1ad831d3ead6ffa16e1b1a
DanielaGonzalez20 <dangoor02@gmail.com>
2026-04-26 19:53:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -552,7 +552,7 @@ with tabs[0]:
             ]}
         ])
 
-    col_tabla, col_interp = st.columns([1, 1])
+    col_tabla, col_interp = st.columns([7, 3])
 
     with col_tabla:
         st.markdown(
