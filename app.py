9086d58af062b99e3b48265c87e45484fa7387f1
DanielaGonzalez20 <dangoor02@gmail.com>
2026-04-26 20:24:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ with tabs[1]:
         font=dict(color='white'),
         xaxis=dict(range=[-5, 105], showgrid=False, visible=False),
         yaxis=dict(range=[-5, 105], showgrid=False, visible=False),
-        margin=dict(t=40, b=10, l=80, r=120),
+        margin=dict(t=40, b=10, l=150, r=150),
         showlegend=False
     )
     st.plotly_chart(fig_map, use_container_width=True)
